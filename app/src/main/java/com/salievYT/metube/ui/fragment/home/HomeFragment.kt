a21ff932e4e7f9b62120e8f59170785dcd381164
salievYT <salievyt@gmail.com>
2024-04-30 13:23:55 +0500

    v1

---
+++ app/src/main/java/com/salievYT/metube/ui/fragment/home/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.salievYT.metube.ui.fragment.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.myself223.metube.R
+import com.myself223.metube.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private val binding : FragmentHomeBinding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
