aa7a829cdf46ac6dcaa7aa3c41f32f8a4fce0a21
salievYT <salievyt@gmail.com>
2024-04-30 19:46:44 +0500

    v1.1

--- app/src/main/java/com/salievYT/metube/ui/activity/noEnternet/NoInternetActivity.kt
+++ app/src/main/java/com/salievYT/metube/ui/activity/noEnternet/NoInternetActivity.kt
@@ -1,4 +1,4 @@
-package com.salievYT.metube.ui
+package com.salievYT.metube.ui.activity.noEnternet
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.myself223.metube.R
 
-class MainActivity : AppCompatActivity() {
+class NoInternetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_no_internet)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
